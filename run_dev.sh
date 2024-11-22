ca3cbd84d0e2676efca3dbf595d9860b3577ac28
Rex <htetnaing0814@gmail.com>
2024-11-22 22:06:39 +0700

    update deployment

--- run_dev.sh
+++ run_dev.sh
@@ -3,16 +3,8 @@
 # Check if the containers are already running
 if [ "$(docker-compose ps -q)" ]; then
   echo "Containers are already running. Starting without rebuild..."
-  docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
+  docker-compose -f docker-compose.yaml up
 else
   echo "Containers are not running. Building and starting..."
-  docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+  docker-compose -f docker-compose.yaml up --build
 fi
-
-# # Set the environment variables
-# export VITE_REACT_APP_GOOGLE_CLIENT_ID=1026550055658-skeaoo2ipej0ntv2i5vtj3s7isgdhqg4.apps.googleusercontent.com
-# export VITE_REACT_APP_SERVER_BASE_URL=http://localhost:3000
-# export VITE_REACT_APP_CLIENT_BASE_URL=http://localhost:5173
-
-# # Run the development server
-# yarn dev
